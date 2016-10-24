c3c500b72caac30e8c4aed32c081fdde00f1a0e2
Cyrus <cdbalsara@icloud.com>
2016-10-24 17:19:39 +0100

    added index, new, create and find routes for merchant

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -1,13 +1,26 @@
 require_relative('../models/merchant')
 
 #index
-
+get '/merchants' do
+  @merchants = Merchant.all
+  erb(:'merchants/index')
+end 
 #new
-
+get '/merchants/new' do
+  erb(:'merchants/new')
+end 
 #create
+post '/merchants' do
+  @merchant = Merchant.new(params)
+  @merchant.save
+  redirect to("merchants")
+  end 
 
-#find
-
+#find/show
+get '/merchants/:id' do
+  @merchant = Merchant.find(params['id'])
+  erb(:'merchants/show')
+  end 
 #edit
 
 #update
